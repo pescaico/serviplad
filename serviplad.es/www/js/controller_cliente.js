fb62fe18fef0f825a275cdb9815d47fbb9b72952
Gaxp <gasparhe@gmail.com>
2014-12-06 11:11:59 +0100

    autorelleneado realizado para cliente y contacoto

--- serviplad.es/www/js/controller_cliente.js
+++ serviplad.es/www/js/controller_cliente.js
@@ -61,10 +61,7 @@ angular.module('starter.controllers')
 
      
     var data = [{id:1,nmPlaca:'IKC-1394'},{id:2,nmPlaca:'IKY-5437'},{id:3,nmPlaca:'IKC-1393'},{id:4,nmPlaca:'IKI-5437'},{id:5,nmPlaca:'IOC-8749'},{id:6,nmPlaca:'IMG-6509'}];
-    $scope.veiculos = data;
-    $scope.testa = function(){
-      alert($scope.veiculo.nmPlaca);
-    }
+    $scope.clientes = data;
 
   $scope.cliente = {
     fecha : new Date(),
@@ -83,6 +80,10 @@ angular.module('starter.controllers')
 $scope.isOk = function() {
   return $scope.billForm.$valid;
 }
+
+$scope.tipoCliente = { placeholder: 'Cliente', address: '1600 Amphitheatre' };
+$scope.tipoContacto = { placeholder: 'Contacto', address: '123 Somewhere' };
+
 
   $scope.goNewClienteForm = function() {
     mySharedService.prepForBroadcast($scope.cliente);
@@ -247,25 +248,38 @@ $scope.isOk = function() {
     scope: {
       label: '@',
       labelField: '@',
+      dataselect: '=',
       provider: '=',
       ngModel: '=?',
       ngValue: '=?',
-
     },
     require: '?ngModel',
     transclude: false,
     replace: false,
-    template: '<div class="selectContainer">' + '<label class="item item-input item-stacked-label">' + '<span class="input-label">{{label}}</span>' + '<div class="item item-input-inset">' + '<label class="item-input-wrapper">' + '<i class="icon ion-ios7-search placeholder-icon"></i>' + '<input id="filtro" type="search"  ng-model="ngModel" ng-value="ngValue" ng-keydown="onKeyDown()"/>' + '</label>' + '<button class="button button-small button-clear" ng-click="open()">' + '<i class="icon ion-chevron-down"></i>' + '</button>' + '</div>' + '</label>' + '<div class="optionList padding-left padding-right" ng-show="showHide">' + '<ion-scroll>' + '<ul class="list">' + '<li class="item" ng-click="selecionar(item)" ng-repeat="item in provider | dynamicFilter:[labelField,ngModel]">{{item[labelField]}}</li>' + '</ul>' + '</ion-scroll>' + '</div>' + '</div>',
-    link: function(scope, element, attrs, ngModel) {
+    template: '<div class="selectContainer">' 
+              + '<label class="item item-input-inset">' 
+                + '<i class="icon ion-person placeholder-icon"></i>' 
+                + '<input type="search" placeholder="{{dataselect.placeholder}}" ng-model="ngModel" ng-value="ngValue" ng-keydown="onKeyDown()">'
+                +'<label class="button button-icon icon ion-chevron-down" ng-click="open()" ></label>'
+                +'<label class="button button-icon icon ion-person-add icon_green" ng-click="goNewPerson()" ></label>'
+              + '</label>'  
+          + '<div class="optionList padding-left padding-right" ng-show="showHide">' 
+            + '<ion-scroll>' 
+              + '<ul class="list">' 
+                  + '<li class="item" ng-click="selecionar(item)" ng-repeat="item in provider | dynamicFilter:[labelField,ngModel]">{{item[labelField]}}</li>' 
+                + '</ul>' 
+            + '</ion-scroll>' 
+          + '</div>' 
+        + '</div>',
+    link: function(scope, element, attrs, ngModel,state) {
       scope.ngValue = scope.ngValue !== undefined ? scope.ngValue : 'item';
       scope.selecionar = function(item) {
         ngModel.$setViewValue(item);
         scope.showHide = false;
       };
-
-      element.bind('click', function() {
-        //element.find('input').focus();
-      });
+      scope.goNewPerson = function() {
+        scope.$parent.goNewClienteForm();
+      }
 
       scope.open = function() {
         scope.ngModel = undefined;

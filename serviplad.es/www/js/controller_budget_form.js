f22e030d2314bb2fe54541cb5e66f7bb62a796dd
unknown <pescaico@gmail.com>
2015-10-21 22:35:43 +0200

    Nuevos ficheros relacionados con el inicio de la aplicación, presupestos y notas

---
+++ serviplad.es/www/js/controller_budget_form.js
@@ -0,0 +1,171 @@
+angular.module('starter.controllers')
+
+.controller('NuevoPresupuestoCtrl', function($scope, $rootScope,  DataService, mySharedService, $stateParams, $ionicPopup, $timeout, $http, $compile, $state) {
+  //$scope.contacto = PresupuestosService.get($stateParams.presupuestoId);
+  $scope.title = "Nuevo Presupuesto";
+  //$scope.focusManager = { focusInputOnBlur: true};
+
+  //var data = [{id:1,nmPlaca:'IKC-1394'},{id:2,nmPlaca:'IKY-5437'},{id:3,nmPlaca:'IKC-1393'},{id:4,nmPlaca:'IKI-5437'},{id:5,nmPlaca:'IOC-8749'},{id:6,nmPlaca:'IMG-6509'}];
+  $scope.clientes = DataService.getList().clientes;
+  $scope.ciudades = DataService.getList().ciudades;
+  $scope.provincias = DataService.getList().provincias;
+  $scope.contactos = DataService.getList().contactos;
+
+  $scope.tipoCliente = { placeholder: 'Cliente', required: true, ngMinLength:4, name:'cliente', type:'text'};
+  $scope.tipoContacto = { placeholder: 'Contacto',required: false, ngMinLength:4, name:'contacto', type:'text'};
+  $scope.tipoCiudad = { placeholder: 'Ciudad', required: true, ngMinLength:4, name:'provincia', type:'text'};
+  $scope.tipoProvincia = { placeholder: 'Provincia', required: true, ngMinLength:4, name:'ciudad', type:'text'};
+
+  $scope.lineas = 0;
+  $scope.numLineas = 0;
+  $scope.presupuesto = mySharedService.message;
+
+
+  $scope.isOk = function() {
+    return $scope.billForm.$valid;
+  }
+
+  $scope.saveAndUpdate = function() {
+    alert('go to save');
+  }
+
+  $scope.submit = function() {
+    $scope.presupuesto.submissions++;
+    $scope.presupuesto.summary = angular.copy($scope.presupuesto.nombre) ;
+  };
+
+  $scope.goNewClienteForm = function() {
+    mySharedService.prepForBroadcast($scope.presupuesto,"cliente");
+    $state.go('app.nuevoCliente');
+  };
+
+  $scope.goNewContactoForm = function() {
+    mySharedService.prepForBroadcast($scope.presupuesto, "contacto");
+    $state.go('app.nuevoCliente');
+  };
+
+  $scope.$on('handleBroadcast', function() {
+    $scope.presupuesto = mySharedService.message;
+  });
+
+  $scope.removeLinea = function(linea) {
+    delete $scope.presupuesto.materiales[linea];
+    $scope.numLineas--;
+
+    $scope.setPastaTotal();
+  };
+
+  $scope.setPastaTotal = function() {
+    var IVA = 0.21;
+    var total = 0;
+    var totalIVA = 0;
+    var ivaAplicado = 0;
+    //$scope.presupuesto.materiales.forEach(function(entry) {
+    for (entry  in $scope.presupuesto.materiales) {
+      var entity = $scope.presupuesto.materiales[entry];
+      total += entity.precio * entity.cantidad;
+    }//);
+    total = Math.round(total*100)/100;
+    ivaAplicado = total * IVA;
+    ivaAplicado = Math.round(ivaAplicado*100)/100;
+    totalIVA = ivaAplicado + total;
+    totalIVA = Math.round(totalIVA*100)/100;
+    angular.element(document.getElementById('total-presupuesto')).html(total + "€");
+    angular.element(document.getElementById('presupuesto-IVA')).html(ivaAplicado + "€");
+    angular.element(document.getElementById('total-presupuesto-IVA')).html(totalIVA + "€");
+  }
+
+$scope.showPopup = function() {
+   $scope.presupuesto.materiales[$scope.lineas+'a']  = {nombre:"",
+    cantidad:"",
+    precio:""};
+   // An elaborate, custom popup
+   var myPopup = $ionicPopup.show({
+     //template: 't_bill_popup.html',
+     templateUrl: 'templates/t_bill_popup.html',
+     title: 'Introduce la linea',
+     //subTitle: 'Please use normal things',
+     scope: $scope,
+     buttons: [
+     { text: 'Cancel',
+     onTap: function(e) {
+       if (!$scope.presupuesto.materiales) {
+             //don't allow the user to close unless he enters wifi password
+             e.preventDefault();
+           } else {
+            //$scope.presupuesto.materiales.pop();
+            delete $scope.presupuesto.materiales[$scope.lineas+'a'];
+            //lineas--;
+            return $scope.presupuesto.materiales;
+
+          }
+        } },
+        {
+         text: '<b>Save</b>',
+         type: 'button-positive',
+         onTap: function(e) {
+           if (!$scope.presupuesto.materiales) {
+             //don't allow the user to close unless he enters wifi password
+             e.preventDefault();
+           } else {
+            var key = $scope.lineas+"a";
+            var IVA = 0.21;
+            var nombre = $scope.presupuesto.materiales[key].nombre;
+            var cantidad = $scope.presupuesto.materiales[key].cantidad;
+            var precioUnidadSinIVA= $scope.presupuesto.materiales[key].precio;
+            var precioUnidadesSinIVA= precioUnidadSinIVA * cantidad;
+            precioUnidadesSinIVA = Math.round(precioUnidadesSinIVA*100)/100;
+            var precioUnidadConIVA= precioUnidadSinIVA*IVA +precioUnidadSinIVA;
+            precioUnidadConIVA = Math.round(precioUnidadConIVA*100)/100;
+            var precioUnidadesConIVA= precioUnidadesSinIVA*IVA +precioUnidadesSinIVA;
+            precioUnidadesConIVA = Math.round(precioUnidadesConIVA*100)/100;
+
+            var linea = nombre + ": " +   cantidad + " uds a " + precioUnidadSinIVA + " €("+precioUnidadConIVA
+              +"€). Total: "+ (precioUnidadesSinIVA) +"€ (con IVA: "+ precioUnidadesConIVA+"€)";
+            angular.element(document.getElementById('lineas-presupuesto')).append(
+              $compile(
+                "<div class='item item-button-right linea_presupuesto'  id='lineas-presupuesto"+$scope.lineas+"'>"
+                + linea
+                + "<a remove-on-click-client ng-click='removeLinea(\""+key+"\")'  id='button-remove-presupuesto"+$scope.lineas+"' "+
+                " class='button button-icon icon ion-close-round icon_red'></a>"
+                +"</div>")($scope));
+            $scope.lineas++;
+            $scope.numLineas++;
+            $scope.setPastaTotal();
+            return $scope.presupuesto.materiales;
+
+          }
+        }
+      },
+      ]
+    });
+};
+})
+.factory('DataService', function() {
+  var listsData =
+  {provincias:[
+    {nombre:'Alicante', id:1},
+    {nombre:'Albacete', id:2},
+    {nombre:'Madrid', id:14}
+    ],
+    ciudades:[
+    {nombre:'Villena', id:2},
+    {nombre:'Caudete', id:4},
+    {nombre:'Tres Cantos', id:88}
+    ],
+    clientes:[
+    {nombre: 'Juan', apellidos:' TH E ', id:1},
+    {nombre: 'Josh', apellidos:'E H E ', id:2},
+    {nombre: 'Juma', apellidos:'ER H E ', id:3},
+    {nombre: 'Anto', apellidos:'A H E ', id:4}
+    ],
+    contactos:[
+    {nombre: 'Paco', apellidos:' cholo ', id:1},
+    {nombre: 'Pepe', apellidos:'niono', id:2},
+    {nombre: 'Pana', apellidos:'bobo ', id:3},
+    {nombre: 'Pere', apellidos:'hgo ', id:4}]
+  }
+ return {
+  getList: function() { return listsData}
+  };
+});
---
+++ serviplad.es/www/js/controller_note_form.js
@@ -0,0 +1,171 @@
+angular.module('starter.controllers')
+
+.controller('NuevaNotaCtrl', function($scope, $rootScope,  DataService, mySharedService, $stateParams, $ionicPopup, $timeout, $http, $compile, $state) {
+  //$scope.contacto = NotasService.get($stateParams.notaId);
+  $scope.title = "Nueva Nota";
+  //$scope.focusManager = { focusInputOnBlur: true};
+
+  //var data = [{id:1,nmPlaca:'IKC-1394'},{id:2,nmPlaca:'IKY-5437'},{id:3,nmPlaca:'IKC-1393'},{id:4,nmPlaca:'IKI-5437'},{id:5,nmPlaca:'IOC-8749'},{id:6,nmPlaca:'IMG-6509'}];
+  $scope.clientes = DataService.getList().clientes;
+  $scope.ciudades = DataService.getList().ciudades;
+  $scope.provincias = DataService.getList().provincias;
+  $scope.contactos = DataService.getList().contactos;
+
+  $scope.tipoCliente = { placeholder: 'Cliente', required: true, ngMinLength:4, name:'cliente', type:'text'};
+  $scope.tipoContacto = { placeholder: 'Contacto',required: false, ngMinLength:4, name:'contacto', type:'text'};
+  $scope.tipoCiudad = { placeholder: 'Ciudad', required: true, ngMinLength:4, name:'provincia', type:'text'};
+  $scope.tipoProvincia = { placeholder: 'Provincia', required: true, ngMinLength:4, name:'ciudad', type:'text'};
+
+  $scope.lineas = 0;
+  $scope.numLineas = 0;
+  $scope.nota = mySharedService.message;
+
+
+  $scope.isOk = function() {
+    return $scope.billForm.$valid;
+  }
+
+  $scope.saveAndUpdate = function() {
+    alert('go to save');
+  }
+
+  $scope.submit = function() {
+    $scope.nota.submissions++;
+    $scope.nota.summary = angular.copy($scope.nota.nombre) ;
+  };
+
+  $scope.goNewClienteForm = function() {
+    mySharedService.prepForBroadcast($scope.nota,"cliente");
+    $state.go('app.nuevoCliente');
+  };
+
+  $scope.goNewContactoForm = function() {
+    mySharedService.prepForBroadcast($scope.nota, "contacto");
+    $state.go('app.nuevoCliente');
+  };
+
+  $scope.$on('handleBroadcast', function() {
+    $scope.nota = mySharedService.message;
+  });
+
+  $scope.removeLinea = function(linea) {
+    delete $scope.nota.materiales[linea];
+    $scope.numLineas--;
+
+    $scope.setPastaTotal();
+  };
+
+  $scope.setPastaTotal = function() {
+    var IVA = 0.21;
+    var total = 0;
+    var totalIVA = 0;
+    var ivaAplicado = 0;
+    //$scope.nota.materiales.forEach(function(entry) {
+    for (entry  in $scope.nota.materiales) {
+      var entity = $scope.nota.materiales[entry];
+      total += entity.precio * entity.cantidad;
+    }//);
+    total = Math.round(total*100)/100;
+    ivaAplicado = total * IVA;
+    ivaAplicado = Math.round(ivaAplicado*100)/100;
+    totalIVA = ivaAplicado + total;
+    totalIVA = Math.round(totalIVA*100)/100;
+    angular.element(document.getElementById('total-nota')).html(total + "€");
+    angular.element(document.getElementById('nota-IVA')).html(ivaAplicado + "€");
+    angular.element(document.getElementById('total-nota-IVA')).html(totalIVA + "€");
+  }
+
+$scope.showPopup = function() {
+   $scope.nota.materiales[$scope.lineas+'a']  = {nombre:"",
+    cantidad:"",
+    precio:""};
+   // An elaborate, custom popup
+   var myPopup = $ionicPopup.show({
+     //template: 't_bill_popup.html',
+     templateUrl: 'templates/t_bill_popup.html',
+     title: 'Introduce la linea',
+     //subTitle: 'Please use normal things',
+     scope: $scope,
+     buttons: [
+     { text: 'Cancel',
+     onTap: function(e) {
+       if (!$scope.nota.materiales) {
+             //don't allow the user to close unless he enters wifi password
+             e.preventDefault();
+           } else {
+            //$scope.nota.materiales.pop();
+            delete $scope.nota.materiales[$scope.lineas+'a'];
+            //lineas--;
+            return $scope.nota.materiales;
+
+          }
+        } },
+        {
+         text: '<b>Save</b>',
+         type: 'button-positive',
+         onTap: function(e) {
+           if (!$scope.nota.materiales) {
+             //don't allow the user to close unless he enters wifi password
+             e.preventDefault();
+           } else {
+            var key = $scope.lineas+"a";
+            var IVA = 0.21;
+            var nombre = $scope.nota.materiales[key].nombre;
+            var cantidad = $scope.nota.materiales[key].cantidad;
+            var precioUnidadSinIVA= $scope.nota.materiales[key].precio;
+            var precioUnidadesSinIVA= precioUnidadSinIVA * cantidad;
+            precioUnidadesSinIVA = Math.round(precioUnidadesSinIVA*100)/100;
+            var precioUnidadConIVA= precioUnidadSinIVA*IVA +precioUnidadSinIVA;
+            precioUnidadConIVA = Math.round(precioUnidadConIVA*100)/100;
+            var precioUnidadesConIVA= precioUnidadesSinIVA*IVA +precioUnidadesSinIVA;
+            precioUnidadesConIVA = Math.round(precioUnidadesConIVA*100)/100;
+
+            var linea = nombre + ": " +   cantidad + " uds a " + precioUnidadSinIVA + " €("+precioUnidadConIVA
+              +"€). Total: "+ (precioUnidadesSinIVA) +"€ (con IVA: "+ precioUnidadesConIVA+"€)";
+            angular.element(document.getElementById('lineas-nota')).append(
+              $compile(
+                "<div class='item item-button-right linea_nota'  id='lineas-nota"+$scope.lineas+"'>"
+                + linea
+                + "<a remove-on-click-client ng-click='removeLinea(\""+key+"\")'  id='button-remove-nota"+$scope.lineas+"' "+
+                " class='button button-icon icon ion-close-round icon_red'></a>"
+                +"</div>")($scope));
+            $scope.lineas++;
+            $scope.numLineas++;
+            $scope.setPastaTotal();
+            return $scope.nota.materiales;
+
+          }
+        }
+      },
+      ]
+    });
+};
+})
+.factory('DataService', function() {
+  var listsData =
+  {provincias:[
+    {nombre:'Alicante', id:1},
+    {nombre:'Albacete', id:2},
+    {nombre:'Madrid', id:14}
+    ],
+    ciudades:[
+    {nombre:'Villena', id:2},
+    {nombre:'Caudete', id:4},
+    {nombre:'Tres Cantos', id:88}
+    ],
+    clientes:[
+    {nombre: 'Juan', apellidos:' TH E ', id:1},
+    {nombre: 'Josh', apellidos:'E H E ', id:2},
+    {nombre: 'Juma', apellidos:'ER H E ', id:3},
+    {nombre: 'Anto', apellidos:'A H E ', id:4}
+    ],
+    contactos:[
+    {nombre: 'Paco', apellidos:' cholo ', id:1},
+    {nombre: 'Pepe', apellidos:'niono', id:2},
+    {nombre: 'Pana', apellidos:'bobo ', id:3},
+    {nombre: 'Pere', apellidos:'hgo ', id:4}]
+  }
+ return {
+  getList: function() { return listsData}
+  };
+});

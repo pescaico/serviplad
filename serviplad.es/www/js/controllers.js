2aba76be51b02f7e51b53eae51826fb428d76565
unknown <pescaico@gmail.com>
2015-10-25 09:51:26 +0100

    no changes

--- serviplad.es/www/js/controllers.js
+++ serviplad.es/www/js/controllers.js
@@ -3,7 +3,7 @@ angular.module('starter.controllers',[])
 .controller('AppCtrl', function($scope, $ionicModal, $timeout) {
   // Form data for the login modal
   $scope.loginData = {};
-  
+
   // Create the login modal that we will use later
   $ionicModal.fromTemplateUrl('templates/login.html', {
     scope: $scope

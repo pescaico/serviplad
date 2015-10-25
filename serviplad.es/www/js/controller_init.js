90740804936905bb081438d0d65e88e6e6b2e75d
unknown <pescaico@gmail.com>
2015-10-25 14:03:22 +0100

    se eliminia myMessageShared

--- serviplad.es/www/js/controller_init.js
+++ serviplad.es/www/js/controller_init.js
@@ -1,6 +1,6 @@
 angular.module('starter.controllers')
 
-.controller('InicioCtrl', function($scope, $rootScope,  DataService, mySharedService, facturaShared, $stateParams, $ionicPopup, $timeout, $http, $compile, $state) {
+.controller('InicioCtrl', function($scope, $rootScope,  DataService, facturaShared, $stateParams, $ionicPopup, $timeout, $http, $compile, $state) {
   //$scope.contacto = PresupuestosService.get($stateParams.presupuestoId);
   $scope.title = "Inicio";
   //$scope.focusManager = { focusInputOnBlur: true};

48d0484f797985423aa3377f1bdbafc58fb9dc5c
unknown <pescaico@gmail.com>
2015-10-25 12:39:48 +0100

    refactorización de factoyr

--- serviplad.es/www/js/factory.js
+++ serviplad.es/www/js/factory.js
@@ -44,4 +44,33 @@ angular.module('starter.controllers')
     };
 
     return sharedFacturaService;
+})
+.factory('DataService', function() {
+  var listsData =
+  {provincias:[
+    {nombre:'Alicante', id:1},
+    {nombre:'Albacete', id:2},
+    {nombre:'Madrid', id:14}
+    ],
+    ciudades:[
+    {nombre:'Villena', id:2},
+    {nombre:'Caudete', id:4},
+    {nombre:'Tres Cantos', id:88}
+    ],
+    clientes:[
+    {nombre: 'Juan', apellidos:' TH E ', id:1, nombreCompleto: 'Juan  (74004106l)'},
+    {nombre: 'Josh', apellidos:'E H E ', id:2, nombreCompleto: 'JOSH 2 (74004106l)'},
+    {nombre: 'Juma', apellidos:'ER H E ', id:3, nombreCompleto: 'JUMA  THE (74004106l)'},
+    {nombre: 'Anto', apellidos:'A H E ', id:4, nombreCompleto: 'Antonio THE (74004106l)'}
+    ],
+    contactos:[
+    {nombre: 'Paco', apellidos:' cholo ', id:1},
+    {nombre: 'Pepe', apellidos:'niono', id:2},
+    {nombre: 'Pana', apellidos:'bobo ', id:3},
+    {nombre: 'Pere', apellidos:'hgo ', id:4}]
+  }
+ return {
+  getList: function() { return listsData}
+  };
 });
+

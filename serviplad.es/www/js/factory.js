4e36bc82f0bec178cd2556b0226a7081ea03877f
unknown <pescaico@gmail.com>
2015-10-25 14:03:57 +0100

    se mete nombre completo

--- serviplad.es/www/js/factory.js
+++ serviplad.es/www/js/factory.js
@@ -1,31 +1,4 @@
 angular.module('starter.controllers')
-.factory('mySharedService', function($rootScope) {
-    var sharedService = {};
-
-    sharedService.message = {
-        fecha : new Date(),
-        direccion : "",
-        provincia: "",
-        ciudad: "",
-        cliente: null,
-        contacto: null,
-        summary:"",
-        materiales: []
-  };
-    sharedService.type = '';
-
-    sharedService.prepForBroadcast = function(msg, type) {
-        this.message = msg;
-        this.type = type;
-        this.broadcastItem();
-    };
-
-    sharedService.broadcastItem = function() {
-        $rootScope.$broadcast('handleBroadcast');
-    };
-
-    return sharedService;
-})
 .factory('facturaShared', function($rootScope) {
     var sharedFacturaService = {};
 
